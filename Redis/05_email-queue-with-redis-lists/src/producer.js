62b53645f77491e70bcdb465d34be7a32ba4711e
unknown <imdadulhoquekk@gmail.com>
2026-09-18 23:06:28 +0530

    email queue write producer logic

--- Redis/05_email-queue-with-redis-lists/src/producer.js
+++ Redis/05_email-queue-with-redis-lists/src/producer.js
@@ -11,21 +11,21 @@ const PORT = process.env.PORT || 3000;
 // email queue with redis Lists
 const QUEUE_KEY = "queue:email";
 
-app.post("/send-email" , async (req , res) => {
+app.post("/send-email", async (req, res) => {
 
 
-    const { to , subject , message } = req.body;
+    const { to, subject, message } = req.body;
 
     const emailJob = {
-        to : to ,
-        subject : subject || "No subject" ,
-        message : message || "No message",
-        createdAt : new Date().toLocaleString()
+        to: to,
+        subject: subject || "No subject",
+        message: message || "No message",
+        createdAt: new Date().toLocaleString()
     }
-    
-    await redis.lpush(QUEUE_KEY , JSON.stringify(emailJob));
 
-    res.json({status : "ok" , message : "Email job added to the queue" , job : emailJob});
+    await redis.lpush(QUEUE_KEY, JSON.stringify(emailJob));
+
+    res.json({ status: "ok", message: "Email job added to the queue", job: emailJob });
 });
 
 
